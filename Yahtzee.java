b368007bf83d9252ea1492eb83f5dbe44273ec2f
Jorimvdw <jorimvanderwijngaard@gmail.com>
2018-10-17 15:28:03 +0200

    Doelstelling 1-9

--- Yahtzee.java
+++ Yahtzee.java
@@ -19,44 +19,136 @@ public class Yahtzee {
 }
 
 class YahtzeeSpel {
-	ArrayList <Dobbelsteen> Dobbelstenen = new ArrayList<>();
+	static ArrayList <Dobbelsteen> dobbelstenen = new ArrayList<>();
+	boolean doorgaan = true;
+	int [] blokkeerarray = new int [5];
+	Speler speler = new Speler();
+	int aantalvervolgworpen = 0;
 	
 	YahtzeeSpel() {
-		for (int y = 0 ; y < 5 ; y++) {
-		Dobbelsteen dobbelsteen = new Dobbelsteen();
-		Dobbelstenen.add(dobbelsteen);		
-		}
+		genereerdobbelstenen(5);
 	}
 	
 	
 	void spelen() {
-		boolean doorgaan = true;
-		Dobbelsteen dobbelsteen = new Dobbelsteen();
-		
-	
-		
 		
 		while (doorgaan) {
 			System.out.println("start spel");
-			for (Dobbelsteen x  : Dobbelstenen ) {
-				System.out.println(x.werpen() +1);				
-			}
 			
-			System.out.println("Wil je nog een potje? enter voor ja, q om te stoppen");
-			if (Yahtzee.sc.nextLine().equalsIgnoreCase("q")) {
-				System.out.println("Exit");
-				doorgaan = false;
-			}
+			
+			laatdobbelstenenzien();
+			vervolgstap();
+			
+		
+			eindespel();
+			
 		} 
+	}
+	
+	void genereerdobbelstenen (int u) {
+		for (int y = 0 ; y < u ; y++) {
+			Dobbelsteen dobbelsteen = new Dobbelsteen();
+			dobbelstenen.add(dobbelsteen);		
+			}
+	}
+	
+	void laatdobbelstenenzien () {
+		for (Dobbelsteen x  : dobbelstenen ) {
+			System.out.println(x.waarde);		
+		}
+	}
+	
+	void vervolgstap () {
+		Worp worp = new Worp();
+		if (aantalvervolgworpen < 2) {
+			System.out.println("Druk 'b' om deze uitkomst te behouden, 'n' om nogmaals te gooien");
+			String invoer = Yahtzee.sc.nextLine();	
+			if (invoer.equalsIgnoreCase("b")) {
+				Worp done = worp.uitslag(worp);
+				speler.worpgeschiedenis.add(done);
+			} else if (invoer.equalsIgnoreCase("n")) {
+				aantalvervolgworpen++;
+				vervolgworp();
+			} else {
+				System.out.println("Ik begrijp niet wat je bedoeld");
+				vervolgstap ();
+			}
+		} else {
+			Worp done = worp.uitslag(worp);
+			speler.worpgeschiedenis.add(done);
+		}
+			
+		
+		
+	}
+
+	void vervolgworp () {
+		System.out.println("");
+		System.out.println("Druk 1 als een dobbelsteen moet blijven liggen, 0 voor opnieuw gooien.");
+		String invoer = Yahtzee.sc.nextLine();
+		
+		for (int u = 0 ; u < 5 ; u++) {				
+			int los = Character.getNumericValue(invoer.charAt(u));
+			blokkeerarray[u] = los;
+		}
+		
+		for (int i = 0 ; i < 5 ; i++) {
+			if (blokkeerarray[i] == 0) { 
+				dobbelstenen.get(i).waarde = Yahtzee.rand.nextInt(6) +1;	
+			}
+		}	
+			
+			laatdobbelstenenzien();
+			vervolgstap ();		
+	}
+	
+	void eindespel() {
+		System.out.println("Wil je nog een potje? enter voor ja, q om te stoppen");
+		String invoer  = Yahtzee.sc.nextLine();
+		if (invoer.equalsIgnoreCase("q")) {
+			System.out.println("Exit");
+			doorgaan = false;
+		} else if (invoer.length() == 0) {
+			System.out.println("Een nieuw spel begint");
+		} else {
+			System.out.println("Verkeerde input");	
+			eindespel();
+		}
 	}
 
 
 }
 
 class Dobbelsteen {
-	int waarde = 3;
+	Dobbelsteen () {
+		werpen();
+	}
+	int waarde = 0;
+	
 	int werpen() {	
-		waarde = Yahtzee.rand.nextInt(6);
+		waarde = Yahtzee.rand.nextInt(6) +1;
 		return waarde;
 	}
+}
+
+class Worp {
+	 int [] uitslag = new int [5]; 
+	
+	Worp uitslag (Worp worp) {
+		for (int b = 0 ; b < 5 ; b++) {
+			worp.uitslag[b] = YahtzeeSpel.dobbelstenen.get(b).waarde;
+		}
+		System.out.println("De einduitslag van deze worp is:");
+		for (int a = 0 ; a < uitslag.length ; a++) {
+			System.out.println(uitslag[a]);
+		}
+		return worp;
+			
+	}
+}
+
+class Speler {
+	ArrayList<Worp> worpgeschiedenis = new ArrayList<>();
+	
+	
 }
